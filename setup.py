10ff50400c4ec08abf0c138cf9fff53441b9380d
Apostolos Bessas <mpessas@transifex.com>
2012-07-19 15:11:06 +0300

    Add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = "django-pgtags",
+    version = "0.1devel",
+    description = "Store tags in PostgreSQL as arrays.",
+    author = "Apostolos Bessas",
+    author_email = "mpessas@gmail.com",
+    packages=["pgtags"]
+)
